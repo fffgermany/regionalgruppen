d4881d4908bf6a40bb0189e72ddb086f690d26c0
Marco Gergele <mg@cantamen.de>
2019-02-24 08:39:33 +0100

    Nutzer ohne Passwort auflisten

--- src/public/index.php
+++ src/public/index.php
@@ -10,8 +10,6 @@ $api = new \Slim\App(['settings'=>$config]);
 
 $container = $api->getContainer();
 
-$user = new User();
-
 $container['logger'] = function($c) {
   $logger = new \Monolog\Logger('my_logger');
   $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
@@ -19,56 +17,59 @@ $container['logger'] = function($c) {
   return $logger;
 };
 
-$container['db'] = function ($c) {
-    $db = $c['settings']['db'];
-    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
-        $db['user'], $db['pass']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $pdo;
-};
-
 R::setup( 'mysql:host='.$config['db']['host'].';dbname='.$config['db']['dbname'], $config['db']['user'], $config['db']['pass']);
+R::freeze( TRUE );
+
+define('USER','user');
+define('ORTSGRUPPE','ortsgruppe');
 
 $api->get('/user', function (Request $request, Response $response, array $args){
-  $query = "select u.id, u.name, u.description, u.email, o.name as ortsgruppe, o.id as ortsgruppe_id from user u left join ortsgruppe o on o.id = u.ortsgruppe";
-  $stmt = $this->db->prepare($query);
-  $stmt->execute();
-  $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  echo json_encode($data);
+  $data = R::findAll(USER);
+  echo json_encode(R::exportAll($data, FALSE, User::$showVars));
+});
+$api->get('/user/{id}', function (Request $request, Response $response, array $args){
+  $data = R::load(USER, $args['id']);
+  $show = User::visible($data->export());
+  echo json_encode($show);
 });
 
 $api->put('/user', function (Request $request, Response $response){
-  $body=$request->getBody();
-  $user = json_decode($body,true);
-  if($user['ortsgruppe'] && ! is_numeric($user['ortsgruppe'])){
-    return $response->withStatus(400)->write("ortsgruppe '" . $user['ortsgruppe'] . "' muss die ID sein!");
+  $content = json_decode($request->getBody(),true);
+  $existing=R::findAll(USER,'email=? and ortsgruppe_id=?', [$content['email'],$content['ortsgruppe_id']]);
+  if(count($existing) > 0){
+    return $response->withStatus(400)->write("Duplikat mit User " . array_shift($existing)->id . "\n");
   }
-  $query = "insert into user (name, description, email, ortsgruppe) values (:name, :description, :email, :ortsgruppe) ";
-  $stmt = $this->db->prepare($query);
-  $stmt->bindParam(":name",$user['name'], PDO::PARAM_STR);
-  $stmt->bindParam(":description",$user['description'], PDO::PARAM_STR);
-  $stmt->bindParam(":email",$user['email'], PDO::PARAM_STR);
-  $stmt->bindParam(":ortsgruppe",$user['ortsgruppe'], PDO::PARAM_INT);
-  $stmt->execute();
+  $insert=R::dispense(USER);
+  $insert->import($content);
+  R::store($insert);
+});
+
+$api->post('/user', function (Request $request, Response $response){
+  $body=$request->getBody();
+  $userData = json_decode($body,true);
+  $existingUser=R::findAll(USER,'email=? and ortsgruppe_id=?', [$userData['email'],$userData['ortsgruppe_id']]);
+  if(count($existingUser) > 0){
+    return $response->withStatus(400)->write("Duplikat mit User " . array_shift($existingUser)->id . "\n");
+  }
+  $user=R::dispense(USER);
+  $user->import($userData);
+  R::store($user);
+
 });
 
 $api->get('/ortsgruppe', function (Request $request, Response $response, array $args){
-  $query = "select * from ortsgruppe ";
-  $stmt = $this->db->prepare($query);
-  $stmt->execute();
-  $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  echo json_encode($data);
+  $data = R::findAll(ORTSGRUPPE);
+  echo json_encode(R::exportAll($data));
 });
 
 $api->get('/ortsgruppe/{og}/user', function (Request $request, Response $response, array $args){
   $og = $args['og'];
-  $query = "select id, name, description, email from user where ortsgruppe = :ortsgruppe";
-  $stmt = $this->db->prepare($query);
-  $stmt->bindParam(":ortsgruppe",$og, PDO::PARAM_INT);
-  $stmt->execute();
-  $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
- echo json_encode($data);
+  $data=R::findAll(USER,'ortsgruppe_id=?', [$og]);
+  echo json_encode($data);
+});
+
+$api->get('/test', function(){
+  echo "wenn ich nischt mache, gehts!";
 });
 
 $api->run();
